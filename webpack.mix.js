084589b5c096dae9db3fa15e888acfd179820a3d
abdilatiifali <abdilatiifali@gmail.com>
2022-04-22 20:27:07 +0300

    add versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,10 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .vue()
+    .version()
     .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
     .alias({ "@": "resources/js" });
+
+if (mix.inProduction()) {
+    mix.version();
+}
