9e9e73bf0880b5880cf9ca59f3bda5c7e2f18723
abdilatiifali <abdilatiifali@gmail.com>
2022-05-04 22:08:31 +0300

    added meta tags and favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,31 @@
 <!DOCTYPE html>
 <html class="h-full">
   <head>
+    <title>Locum Pap - We connect healthcare workers to medical facilities countrywide.</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <meta name="title" content="Locum Pap connects healthcare workers to medical facilities countrywide.">
+    <meta name="description" content="Our mission is connecting healthcare workers to medical facilities countrywide. Our vision is to bridge the existing gap in the medical sector through offering real time human resource assistance where needed.">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://cocky-bell-2406e2.netlify.app/">
+    <meta property="og:title" content="Locum Pap connects healthcare workers to medical facilities countrywide.">
+    <meta property="og:description" content="Our mission is connecting healthcare workers to medical facilities countrywide. Our vision is to bridge the existing gap in the medical sector through offering real time human resource assistance where needed.">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://cocky-bell-2406e2.netlify.app/">
+    <meta property="twitter:title" content="Locum Pap connects healthcare workers to medical facilities countrywide.">
+    <meta property="twitter:description" content="Our mission is connecting healthcare workers to medical facilities countrywide. Our vision is to bridge the existing gap in the medical sector through offering real time human resource assistance where needed.">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/favicons/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="preload" href="/heroimage.jpeg" as="image">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
