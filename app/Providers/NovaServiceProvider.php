2e46596c5946e8085782c423ed1686bdd0a076fe
abdilatiifali <abdilatiifali@gmail.com>
2022-07-11 19:04:57 +0300

    add another admin user

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -44,6 +44,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
                 'abdilatiifali@gmail.com',
+                'locumpap@gmail.com',
             ]);
         });
     }
